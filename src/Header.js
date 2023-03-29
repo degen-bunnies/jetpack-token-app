d402f5843a77a5f52379152a4dbf175b6a042323
byunghwa.yun <combine@combineads.co.kr>
2023-03-29 22:23:39 +0900

    Change size

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,15 @@ function Header(props) {
     <div className="header">
       <div className="header-content">
         <img src={props.imageSrc} alt={props.imageAlt}/>
-        <h1>{props.text}</h1>
-        <h2>
-          <div>Total supply: 1,000,000,000,000,000 Pack</div>
-          <div>Minimum unit: 1 Pack</div>
-          <div>1 Jet = 1,000,000 Packs</div>
-        </h2>
-        <button className="fund-button">Request Funds (Coming Soon)</button>
+        <div className="tokenomics">
+          <h1>{props.text}</h1>
+          <h2>
+            <div>Total supply: 1,000,000,000,000,000 Pack</div>
+            <div>Minimum unit: 1 Pack</div>
+            <div>1 Jet = 1,000,000 Packs</div>
+          </h2>
+          <button className="fund-button">Request Funds (Coming Soon)</button>
+        </div>
       </div>
     </div>
   );
