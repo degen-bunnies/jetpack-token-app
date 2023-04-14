98fcabfed2125c678e1640f06dca3a75c30a4788
combineads <combine@combineads.co.kr>
2023-04-14 14:54:18 +0900

    Add image size information

--- src/Header.js
+++ src/Header.js
@@ -3,30 +3,21 @@ import './Header.css';
 import jetpackSite from './jetpack_site.png';
 
 function Header(props) {
-  const jetpackSiteImgPreload = () => {
-    let img = new Image();
-    img.src = jetpackSite;
-  };
-
-  useLayoutEffect(() => {
-    jetpackSiteImgPreload();
-  }, []);
-
   return (
-    <div className="header">
-      <div className="header-content">
-        <img className="landing-image" src={jetpackSite} alt="Jet pack landing background"/>
-        <div className="tokenomics">
-          <h1>{props.text}</h1>
-          <h2>
-            <div>Total supply: 1,000,000,000,000,000 Pack</div>
-            <div>Minimum unit: 1 Pack</div>
-            <div>1 Jet = 1,000,000 Packs</div>
-          </h2>
-          <button className="fund-button">Request Funds (Coming Soon)</button>
+      <div className="header">
+        <div className="header-content">
+          <img className="landing-image" src={jetpackSite} alt="Jet pack landing background"/>
+          <div className="tokenomics">
+            <h1>{props.text}</h1>
+            <h2>
+              <div>Total supply: 1,000,000,000,000,000 Pack</div>
+              <div>Minimum unit: 1 Pack</div>
+              <div>1 Jet = 1,000,000 Packs</div>
+            </h2>
+            <button className="fund-button">Request Funds (Coming Soon)</button>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
